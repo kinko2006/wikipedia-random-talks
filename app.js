76ef7a1d280a71a6e01b30bf8b3204f17f407c46
kinko2006 <rafael.alfalci@gmail.com>
2024-05-28 08:25:41 -0300

    a

--- app.js
+++ app.js
@@ -1,44 +1,54 @@
-function fetchRandomTalk() {
-    fetch('https://en.wikipedia.org/api/rest_v1/page/random/title')
-    .then(response => response.json())
-    .then(data => {
-        const pageTitle = data.items[0].title;
-        const resultElement = document.createElement('li');
-        resultElement.textContent = pageTitle;
-        document.getElementById('results').appendChild(resultElement);
-        var linkWiki = 'https://en.wikipedia.org/wiki/' + pageTitle;
-    const objj = Object.assign(document.getElementById('results'), {
-    innerHTML: `<div class="spotify">
-    <a href=${linkWiki}><h1>${pageTitle}</h1></a> 
-    
-    
-    
-    </div>`
-});
-            
-    })
-    .catch(error => console.error('error:', error));
-}
-
-function asdasfa() {
-    
-
-}
-
-
-function showValue(){
+function showValue() {
     var rangeInput = document.getElementById("myRange");
     var value = rangeInput.value;
     document.getElementById("varhone").innerText = value;
 }
 
 function updateValue() {
-     var rangeInput = document.getElementById("myRange");
-     var value = rangeInput.value;
-     console.log("valor:" + value)
-     
+    var rangeInput = document.getElementById("myRange");
+    var value = rangeInput.value;
+    console.log("valor:" + value);
+ 
+    const elements = [];
+    for (let i = 0; i < value; i++) {
+        
+}
+
+function updateValue() {
+    var rangeInput = document.getElementById("myRange");
+    var value = rangeInput.value;
+    console.log("valor:" + value)
     for (let i = 0; i < value; i++) {
         fetchRandomTalk();
-
     }
+}
+
+
+function fetchWiki(){
+fetch('https://en.wikipedia.org/api/rest_v1/page/random/title')
+    .then(response => response.json())
+    .then(data => {
+        const pageTitle = data.items[0].title;
+        
+        var linkWiki = 'https://en.wikipedia.org/wiki/' + pageTitle;
+
+        const element = document.createElement('div');
+        element.classList.add('wiki');
+        element.innerHTML = `
+            <a href="${linkWiki}">
+                <h1>${pageTitle}</h1>
+            </a> 
+        `;
+        elements.push(element);
+    })
+     const resultsContainer = document.getElementById('results');
+     elements.forEach(element => {
+         resultsContainer.appendChild(element);
+     });
+        
+        // resultElement.textContent = pageTitle;
+        // document.getElementById('results').appendChild(resultElement);
+    
+    }
+    .catch(error => console.error('error:', error));
 }
