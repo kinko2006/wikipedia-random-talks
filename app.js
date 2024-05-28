e2760d64c243033ad35207c5157d0996270d8254
kinko2006 <rafael.alfalci@gmail.com>
2024-05-28 08:57:55 -0300

    a

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
-function showValue() {
-    var rangeInput = document.getElementById("myRange");
-    var value = rangeInput.value;
-    document.getElementById("varhone").innerText = value;
-}
-
-function updateValue() {
-    var rangeInput = document.getElementById("myRange");
-    var value = rangeInput.value;
-    console.log("valor:" + value);
- 
-    const elements = [];
-    for (let i = 0; i < value; i++) {
-        
-}
-
-function updateValue() {
-    var rangeInput = document.getElementById("myRange");
-    var value = rangeInput.value;
-    console.log("valor:" + value)
-    for (let i = 0; i < value; i++) {
-        fetchRandomTalk();
-    }
-}
-
-
-function fetchWiki(){
+function fetchWiki() {
 fetch('https://en.wikipedia.org/api/rest_v1/page/random/title')
     .then(response => response.json())
     .then(data => {
         const pageTitle = data.items[0].title;
-        
         var linkWiki = 'https://en.wikipedia.org/wiki/' + pageTitle;
 
         const element = document.createElement('div');
@@ -39,16 +12,33 @@ fetch('https://en.wikipedia.org/api/rest_v1/page/random/title')
                 <h1>${pageTitle}</h1>
             </a> 
         `;
+        
         elements.push(element);
     })
+    
      const resultsContainer = document.getElementById('results');
+     const elements = [];
      elements.forEach(element => {
          resultsContainer.appendChild(element);
-     });
-        
-        // resultElement.textContent = pageTitle;
-        // document.getElementById('results').appendChild(resultElement);
-    
+     })
+        .catch(error => console.error('error:', error))
     }
-    .catch(error => console.error('error:', error));
+
+
+
+function updateValue() {
+    var rangeInput = document.getElementById("myRange");
+    var value = rangeInput.value;
+    console.log("valor:" + value);
+        for (let i = 0; i < value; i++) 
+        {
+        fetchWiki();
+    }
+
+}
+
+function showValue() {
+    var rangeInput = document.getElementById("myRange");
+    var value = rangeInput.value;
+    document.getElementById("varhone").innerText = value;
 }
